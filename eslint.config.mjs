af0013d9ca033411c198044c29c78660b693423f
Łukasz <lukasz.nieweglowski86@gmail.com>
2025-06-05 16:32:33 +0200

    chore: eslint ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,13 @@ import tseslint from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
+    {
+        ignores: [
+            'package-lock.json',
+            'playwright-report/**',
+            'test-results/**',
+        ],
+    },
     { files: ['**/*.ts'], plugins: { js }, extends: ['js/recommended'] },
     {
         files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
